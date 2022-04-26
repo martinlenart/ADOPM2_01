67885a7c1955809905742937e0ae599a6a4fc412
Martin <martin_lenart@icloud.com>
2022-04-26 14:06:24 +0200

    Update

---
+++ GameProduct/IInventory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GameProduct
+{
+	public interface IInventory
+	{
+		public List<GameProduct> Inventory { get; set; }
+		public int StockCount { get; }
+		public decimal StockValue { get; }
+	}
+}
+
