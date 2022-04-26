1c838d9145c515f5c869d76423b0407a1fc3c252
Martin <martin_lenart@icloud.com>
2022-04-26 14:16:25 +0200

    Update

--- GameProduct/IInventory.cs
+++ GameProduct/IInventory.cs
@@ -3,10 +3,27 @@ namespace GameProduct
 {
 	public interface IInventory
 	{
-		public List<GameProduct> Inventory { get; set; }
+		/// <summary>
+        /// A list of maximum 10_000 GameProducts
+        /// </summary>
+		public List<GameProduct> InventoryList { get; set; }
 
+		/// <summary>
+        /// Total number of products on Stock
+        /// </summary>
 		public int StockCount { get; }
+
+		/// <summary>
+        /// Total Value of products on Stock
+        /// </summary>
 		public decimal StockValue { get; }
+
+		/// <summary>
+        /// Removes a prod using IEquatable from the InventoryList
+        /// </summary>
+        /// <param name="prod">Product to be removed</param>
+        /// <returns>True if successful, otherwise false</returns>
+		public bool Remove(GameProduct prod);
 	}
 }
 
